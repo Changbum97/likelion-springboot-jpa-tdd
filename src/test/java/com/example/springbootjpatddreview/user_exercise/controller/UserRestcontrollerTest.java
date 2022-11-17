3c7072d4ab2b4704572906b94c93c1df393b13b1
Changbum97 <chb2005@naver.com>
2022-11-17 16:18:27 +0900

    POST /api/v1/users Test

--- src/test/java/com/example/springbootjpatddreview/user_exercise/controller/UserRestcontrollerTest.java
+++ src/test/java/com/example/springbootjpatddreview/user_exercise/controller/UserRestcontrollerTest.java
@@ -3,17 +3,21 @@ package com.example.springbootjpatddreview.user_exercise.controller;
 import com.example.springbootjpatddreview.user_exercise.domain.dto.UserAddRequest;
 import com.example.springbootjpatddreview.user_exercise.domain.dto.UserResponse;
 import com.example.springbootjpatddreview.user_exercise.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +30,9 @@ class UserRestcontrollerTest {
 
     @MockBean
     UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     @DisplayName("GET Single User Test")
@@ -62,5 +69,29 @@ class UserRestcontrollerTest {
                 .andDo(print());
 
         verify(userService).findSingleUser(100L);
+    }
+
+    @Test
+    @DisplayName("POST user Test")
+    void addUserTest() throws Exception {
+
+        UserAddRequest request = new UserAddRequest("username11", "123");
+
+        UserResponse expectedResponse = UserResponse.builder()
+                .id(1L).username("username11").message("가입이 완료 되었습니다.").build();
+
+        given(userService.addUser(any())).willReturn(expectedResponse);
+
+        byte[] jsonRequest = objectMapper.writeValueAsBytes(request);
+
+        mockMvc.perform(post("/api/v1/users").content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.username").value("username11"))
+                .andExpect(jsonPath("$.message").value("가입이 완료 되었습니다."))
+                .andDo(print());
+
+        verify(userService).addUser(any());
     }
 }
