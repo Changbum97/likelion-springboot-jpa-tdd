a3d9ed8b50f4f710a478b583d2c2017687c84743
Changbum97 <chb2005@naver.com>
2022-11-17 15:45:44 +0900

    User Service Test

--- src/main/java/com/example/springbootjpatddreview/user_exercise/domain/dto/UserAddRequest.java
+++ src/main/java/com/example/springbootjpatddreview/user_exercise/domain/dto/UserAddRequest.java
@@ -1,10 +1,12 @@
 package com.example.springbootjpatddreview.user_exercise.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class UserAddRequest {
     private String username;
     private String password;
