4dfd7b5c9ad10425473ed057886fbab159dbbec3
Changbum97 <chb2005@naver.com>
2022-11-17 14:50:25 +0900

    GET /api/v1/users Test

--- src/test/java/com/example/springbootjpatddreview/user_exercise/controller/UserRestcontrollerTest.java
+++ src/test/java/com/example/springbootjpatddreview/user_exercise/controller/UserRestcontrollerTest.java
@@ -1,4 +1,48 @@
+package com.example.springbootjpatddreview.user_exercise.controller;
+
+import com.example.springbootjpatddreview.user_exercise.domain.dto.UserAddRequest;
+import com.example.springbootjpatddreview.user_exercise.domain.dto.UserResponse;
+import com.example.springbootjpatddreview.user_exercise.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserRestcontroller.class)
 class UserRestcontrollerTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Test
+    @DisplayName("GET Single User Test")
+    void getUserTest() throws Exception {
+
+        UserResponse expectedResponse = UserResponse.builder()
+                .id(1L).username("User").build();
+
+        given(userService.findSingleUser(1L)).willReturn(expectedResponse);
+
+        mockMvc.perform(get("/api/v1/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.username").value("User"))
+                .andExpect(jsonPath("$.message").isEmpty())
+                .andDo(print());
+
+        verify(userService).findSingleUser(1L);
+    }
 }
